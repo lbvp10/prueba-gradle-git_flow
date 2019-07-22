11548f268cf794dd3f7d360242523511e9dd106c
lvallep <lvallep@indracompany.com>
2019-07-22 17:19:32 -0500

    102

--- src/main/java/com/gradle/pruebas/pruebas/Configuration.java
+++ src/main/java/com/gradle/pruebas/pruebas/Configuration.java
@@ -2,10 +2,14 @@ package com.gradle.pruebas.pruebas;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 
 @Configurable
 public class Configuration {
-    private ObjectMapper objectMapper(){
+    @Bean
+    public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
+
+
 }
--- src/main/java/com/gradle/pruebas/pruebas/PersonaController.java
+++ src/main/java/com/gradle/pruebas/pruebas/PersonaController.java
@@ -1,5 +1,6 @@
 package com.gradle.pruebas.pruebas;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.List;
 
 
 @RestController(value = "/persona")
+@Log4j
 public class PersonaController {
 
     @GetMapping()
